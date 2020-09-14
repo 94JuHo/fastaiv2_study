22020d13b7cebed9e7972d593047be07f805c6e7
94JuHo <jjs1005k@naver.com>
2020-09-14 22:57:11 +0900

    Add 01_intro_code.py

---
+++ 01_intro.py
@@ -0,0 +1,17 @@
+from fastai.vision.all import *
+path = untar_data(URLs.PETS)/'images'
+
+def is_cat(x): return x[0].isupper()
+dls = ImageDataLoaders.from_name_func(
+    path, get_image_files(path), valid_pct=0.2, seed=42,
+    label_func=is_cat, item_tfms=Resize(224))
+
+learn = cnn_learner(dls, resnet34, metrics=error_rate)
+learn.fine_tune(1)
+
+uploader = SimpleNamespace(data = ['images/chapter1_cat_example.jpg'])
+img = PILImage.create(uploader.data[0])
+is_cat, _, probs = learn.predict(img)
+print(f"Is this a cat?: {is_cat}.")
+print(f"Probability it's a cat: {probs[1].item():.6f}")
+
